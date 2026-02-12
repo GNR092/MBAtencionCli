e42378340ab9cfb1b8069b8b841bee4d38e2c443
GNR092 <genercan092@gmail.com>
2026-02-12 11:28:32 -0600

    Reestructura de las migraciones
    Se cambia la estructura de las migraciones y se agregan 2 tablas

---
+++ database/migrations/2026_02_12_172511_create_user_proyectos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_proyectos', function (Blueprint $table) {
+            $table->id('id_user_p'); // Como se indica en el diagrama
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_proyecto');
+            $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('id_proyecto')->references('id_proyecto')->on('proyectos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_proyectos');
+    }
+};
